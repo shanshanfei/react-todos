203d3604a16e44fc8ab61fabbb77a142660c3f65
费珊珊 <feishanshan@feishanshandeMacBook-Pro.local>
2016-10-28 13:46:34 +0800

    初步

--- app/components/index.js
+++ app/components/index.js
@@ -1,11 +1,13 @@
 //程序主入口
-import './Hello/index.less';
-import './World/index.less';
+// import './Hello/index.less';
+// import './World/index.less';
 
 //ES6语法import
-import Hello from './Hello/index.js';
-import World from './World/index.js';
+// import Hello from './Hello/index.js';
+// import World from './World/index.js';
 
 //nodejs require，与import作用相同
 // require('./Hello/index.js');
 // require('./World/index.js');
+
+import App from './Todos/App.js';
